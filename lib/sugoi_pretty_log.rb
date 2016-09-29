a5816d76723900b475f4565a88cd62df19706492
jiikko <n905i.1214@gmail.com>
2016-09-30 00:30:34 +0900

    rename and summrized useragent

--- lib/sugoi_pretty_log.rb
+++ lib/sugoi_pretty_log.rb
@@ -1,6 +1,46 @@
 require "sugoi_pretty_log/version"
+require "awesome_print"
+require "user_agent_parser"
+require "json"
 
 module SugoiPrettyLog
+  class Parser
+    def initialize(log, options)
+      @log = log.strip
+      @options = options
+      @user_agent_key_name = options[:user_agent].to_s
+    end
+
+    def parse
+      json = build_json
+      unless @user_agent_key_name.empty?
+        json['user_agent'] =
+          UserAgentParser.parse(json[@user_agent_key_name]).to_s
+      end
+      json
+    end
+
+    def build_json
+      unless json?
+        raise(JSON::ParserError, 'not json')
+      end
+      JSON.parse(@log)
+    rescue JSON::ParserError
+      puts '@@@@ done eval @@@@'
+      JSON.parse(eval(@log).to_json)
+    end
+
+    def json
+    end
+
+    private
+
+    def json?
+      /\A{.*}\z/
+    end
+  end
+
   def self.parse(log, options)
+    Parser.new(log, options).parse
   end
 end
--- sugoi_pretty_log.gemspec
+++ sugoi_pretty_log.gemspec
@@ -1,4 +1,3 @@
-# coding: utf-8
 lib = File.expand_path('../lib', __FILE__)
 $LOAD_PATH.unshift(lib) unless $LOAD_PATH.include?(lib)
 require 'sugoi_pretty_log/version'
@@ -23,7 +22,8 @@ Gem::Specification.new do |spec|
   spec.add_dependency "awesome_print"
   spec.add_dependency "user_agent_parser"
 
-  spec.add_development_dependency "bundler", "~> 1.13"
-  spec.add_development_dependency "rake", "~> 10.0"
-  spec.add_development_dependency "rspec", "~> 3.0"
+  spec.add_development_dependency "bundler"
+  spec.add_development_dependency "rake"
+  spec.add_development_dependency "rspec"
+  spec.add_development_dependency "pry"
 end
